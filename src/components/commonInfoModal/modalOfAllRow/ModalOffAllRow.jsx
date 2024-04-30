c08d8a76235bc694c16096b109e43a9b63cbbdb7
azat <arykov.azat.kg@gmail.com>
2024-04-30 17:54:50 +0600

    add: bg image to ModalOfAllRow

--- src/components/commonInfoModal/modalOfAllRow/ModalOffAllRow.jsx
+++ src/components/commonInfoModal/modalOfAllRow/ModalOffAllRow.jsx
@@ -3,18 +3,6 @@ import Modal from "@mui/material/Modal";
 import { RatingStars } from "../../ratings/Ratings";
 import { Box } from "@mui/material";
 
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: "90%",
-  bgcolor: "background.paper",
-  border: "none",
-  boxShadow: 24,
-  p: 4,
-};
-
 export const ModalOfAllRow = ({ open, handleClose, selectedFilm }) => {
   return (
     <div>
@@ -24,24 +12,59 @@ export const ModalOfAllRow = ({ open, handleClose, selectedFilm }) => {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: "90%",
+            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url('https://media.themoviedb.org/t/p/w1920_and_h800_multi_faces/${selectedFilm.backdrop_path}')`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundRepeat: "no-repeat",
+            border: "none",
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <Typography
+            sx={{ color: "#fff" }}
+            id="modal-modal-title"
+            variant="h6"
+            component="h2"
+          >
             {selectedFilm?.title}
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          <Typography
+            id="modal-modal-description"
+            sx={{ mt: 2, color: "#fff" }}
+          >
             {selectedFilm?.overview}
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          <Typography
+            id="modal-modal-description"
+            sx={{ mt: 2, color: "#fff" }}
+          >
             Дата выхода: {selectedFilm?.release_date}
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          <Typography
+            id="modal-modal-description"
+            sx={{ mt: 2, color: "#fff" }}
+          >
             Просмотрено: {selectedFilm?.vote_count}
           </Typography>
-          <Box id="modal-modal-description" sx={{ mt: 2, display: "flex" }}>
+          <Box
+            id="modal-modal-description"
+            sx={{ mt: 2, display: "flex", color: "#fff" }}
+          >
             Рейтинг: <RatingStars rating={selectedFilm?.vote_average} />
           </Box>
 
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          <Typography
+            id="modal-modal-description"
+            sx={{ mt: 2, color: "#fff" }}
+          >
             Страна: ({selectedFilm?.original_language.toUpperCase()})
           </Typography>
           <span
